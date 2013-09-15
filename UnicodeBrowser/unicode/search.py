26d9be0e8eebff304fe4baf743e60055351c16a0
Xiaomao Chen <xiaomao5@live.com>
2013-09-15 12:36:05 -0400

    Added better sorting.

--- UnicodeBrowser/unicode/search.py
+++ UnicodeBrowser/unicode/search.py
@@ -1,9 +1,32 @@
 from django.core.cache import cache
 from UnicodeBrowser.unicode.models import CodePoint
-import re
+from django.conf import settings
+
 from itertools import chain
+from functools import partial
 
 __author__ = 'xiaomao'
+
+better_sort = getattr(settings, 'BETTER_SORT', True)
+
+
+def get_bigrams(string):
+    """
+    Takes a string and returns a list of bigrams
+    """
+    s = string.lower()
+    return {s[i:i+2] for i in xrange(len(s) - 1)}
+
+
+def string_similarity(str1, str2):
+    """
+    Perform bigram comparison between two strings
+    and return a percentage match in decimal form
+    """
+    pairs1 = get_bigrams(str1)
+    pairs2 = get_bigrams(str2)
+    intersection = pairs1 & pairs2
+    return (2.0 * len(intersection)) / (len(pairs1) + len(pairs2))
 
 
 def search(keyword, start=None, limit=None):
@@ -28,7 +51,15 @@ def search(keyword, start=None, limit=None):
 
     noncjk = query.all().exclude(block__name__contains='cjk')
     cjk = query.all().filter(block__name__contains='cjk').order_by('block__name')
-    points = list(chain(noncjk, cjk))
+    if better_sort:
+        sorter = lambda point: round(string_similarity(keyword, point.description), 1)
+        points = list(noncjk)
+        points.sort(key=sorter, reverse=True)
+        cjk = list(cjk)
+        cjk.sort(key=sorter, reverse=True)
+        points.extend(cjk)
+    else:
+        points = list(chain(noncjk, cjk))
 
     cache.set(key, points, None)
     return points[piece]
